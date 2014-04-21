7edfb8a662bbeb2f27f817ea16da440dfe4a8764
Igor Shults <igor.shults@objectpartners.com>
2014-04-21 08:56:43 -0500

    Initial commit of script

---
+++ gitTestModified.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+usage(){
+    echo "Usage: `basename $0` [-h] [-r] [-t testOptions] [<directory>...]\n"
+    echo '    -h      Show this help message.'
+    echo '    -r      Remove the default directories, and only use the ones provided.  Without the flag, the provided directories will be appended to the default list.'
+    echo '    -t      Options to pass to "grails test-app". Default: none.'
+    echo '            See: http://grails.org/doc/latest/ref/Command%20Line/test-app.html'
+}
+
+# Parse the arguments
+testOpts=
+directories='grails-app src/groovy'
+
+while getopts “hrt:” option
+do
+    case ${option} in
+        h) usage; exit 0;;
+        r) directories='';;
+        t) testOpts=${OPTARG};;
+        *) usage; exit 1;;
+    esac
+done
+
+shift $(( OPTIND-1 ))
+directories+=" ${*}"
+
+# Get the modified files
+modifiedFiles=`git diff --name-only HEAD ${directories}`
+regex="([^/]*)\.groovy"
+testPatterns=''
+
+for filename in ${modifiedFiles}; do
+    if [[ ${filename} =~ ${regex} ]]; then
+        testPatterns+="${BASH_REMATCH[1]}* "
+    fi
+done
+
+# Run the tests
+if [ -n "${testPatterns}" ]; then
+    echo "Calling... grails test-app ${testOpts} ${testPatterns}\n"
+
+    grails test-app ${testOpts} ${testPatterns}
+else
+  echo "No modified classes found to test."
+fi
